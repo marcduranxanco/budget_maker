7e46db0231fc94a9acf2519782266239473892e3
mduranx <mduranx@hiberus.com>
2024-04-22 18:36:44 +0200

    EVENTOS - Refactor para aplicar la lógica a las diferentes piezas creadas
    Se procede a utilizar juntos el Suscriber, el Event, la entity SolicitudPresupuesto y el servicio de envío de emails creado
    
    En el archivo PresupuestoEventSubscriber.php:
    
    - Se ha agregado una nueva propiedad $emailService a la clase PresupuestoEventSubscriber. Esta propiedad se utiliza para inyectar una instancia del servicio de correo electrónico (EmailService) en el constructor de la clase.
    - En el constructor, se asigna la instancia del servicio de correo electrónico a la propiedad $emailService.
    - En el método onPresupuestoSolicitado, se utiliza el servicio de correo electrónico ($emailService) para enviar correos electrónicos a los comerciales y al solicitante del presupuesto, utilizando los datos del presupuesto obtenidos del evento.
    
    En el archivo PresupuestoSolicitadoEvent.php:
    
    - Se ha agregado una nueva propiedad $solicitudPresupuesto a la clase PresupuestoSolicitadoEvent. Esta propiedad representa la solicitud de presupuesto asociada al evento.
    - Se ha modificado el constructor para aceptar una instancia de SolicitudPresupuesto y asignarla a la propiedad $solicitudPresupuesto.
    - Se ha modificado el método getPresupuesto para que devuelva la instancia de SolicitudPresupuesto.

--- app/budget_maker/src/Event/PresupuestoSolicitadoEvent.php
+++ app/budget_maker/src/Event/PresupuestoSolicitadoEvent.php
@@ -7,4 +7,14 @@ use App\Event\PresupuestoEvent;
 class PresupuestoSolicitadoEvent extends PresupuestoEvent
 {
     public const NAME = 'presupuesto.solicitado';
+
+    private $solicitudPresupuesto;
+
+    public function __construct(SolicitudPresupuesto $solicitudPresupuesto) {
+        $this->solicitudPresupuesto = $solicitudPresupuesto;
+    }
+
+    public function getPresupuesto(): SolicitudPresupuesto {
+        return $this->solicitudPresupuesto;
+    }
 }
--- app/budget_maker/src/EventSubscriber/PresupuestoEventSubscriber.php
+++ app/budget_maker/src/EventSubscriber/PresupuestoEventSubscriber.php
@@ -10,9 +10,11 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class PresupuestoEventSubscriber implements EventSubscriberInterface
 {
     private $presupuesto;
+    private $emailService;
 
-    public function __construct()
+    public function __construct(EmailService $emailService)
     {
+        $this->emailService = $emailService;
         $this->presupuesto = 'ObjetoPresupuesto';
         /* TODO: Cuando esté creada la entidad presupuesto,
          tendremos que añadirla y así trabajar con sus datos en el evento */
@@ -35,11 +37,14 @@ class PresupuestoEventSubscriber implements EventSubscriberInterface
     {
         // ... Lógica de envío de correos
         $presupuesto = $event->getPresupuesto();
+        $this->emailService->enviarCorreosSolicitudPresupuestoAComerciales($presupuesto);
+        $this->emailService->enviarCorreosSolicitudPresupuestoASolicitante($presupuesto);
     }
 
     public function onPresupuestoCreado(PresupuestoSolicitadoEvent $event) : void
     {
         // ... Lógica de envío de correos
         $presupuesto = $event->getPresupuesto();
+
     }
 }
